d4dca9f04e6e8dfd1aa1757ae48c91b2204c44ba
J3r3mC <j.corbeilla@outlook.fr>
2023-06-08 00:51:44 +0200

    IN PROGRESS

--- src/App.js
+++ src/App.js
@@ -14,44 +14,44 @@ import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   const { token, setToken } = useToken();
 
-  if(!token) {
-      return <Login setToken={setToken} />      
+  if (!token) {
+    return <Login setToken={setToken} />
   }
-  
+
   return (
-    <div className='container'>
-    <nav className="navbar navbar-expand">
-      <Link to={"/tutorials"} className="navbar-brand sub nav-item">
-        J3r3mC
-      </Link>
-      <div className="navbar-nav mr-auto">
-        <li className="nav-item">
-          <Link to={"/tutorials"} className="nav-link">
-            Tutorials
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to={"/add"} className="nav-link">
-            Add
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to={"/auth/register"} className="nav-link">
-            Register 
-          </Link>
-        </li>
-      </div>
-    </nav>
-    <Routes>
-      <Route path = "/"   element = {<TutorialsList />} />
-      <Route path = "/tutorials" className="tuto"   element = {<TutorialsList />} />
-      <Route path = "/add"         element = {<AddTutorial />} />
-      <Route path = "/dashboard"   element = {<Dashboard />} />
-      <Route path = "/preferences" element = {<Preferences />} />
-      <Route path = "/tutorials/:id" element = {<Tutorial />} />
-      <Route path = "/auth/register" element = {<Register />} />
-      <Route path = "/login" className="logginStyle" element = {<Login />} />
-    </Routes>
+    <div class="menuItem">
+      <nav className="navbar navbar-expand">
+        <Link to={"/tutorials"} className="navbar-brand sub nav-item">
+          J3r3mC
+        </Link>
+        <div className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <Link to={"/tutorials"} className="nav-link">
+              Tutorials
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to={"/add"} className="nav-link">
+              Add
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to={"/auth/register"} className="nav-link">
+              Register
+            </Link>
+          </li>
+        </div>
+      </nav>
+      <Routes>
+        <Route path="/" element={<TutorialsList />} />
+        <Route path="/tutorials" className="tuto" element={<TutorialsList />} />
+        <Route path="/add" element={<AddTutorial />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/preferences" element={<Preferences />} />
+        <Route path="/tutorials/:id" element={<Tutorial />} />
+        <Route path="/auth/register" element={<Register />} />
+        <Route path="/login" className="logginStyle" element={<Login />} />
+      </Routes>
     </div>
   );
 }
